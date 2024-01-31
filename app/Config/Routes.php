4e0b9f35e339d954c9616328ec356e0e98459c4c
ra212898 <adrienrovira@gmail.com>
2024-01-31 10:51:23 +0100

    Ajout Login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Pages::Accueil');
+$routes->get('/', 'Pages::Connexion');
+$routes->get('/connexion', 'Pages::Connexion');
+$routes->post('/connexion', 'ConnexionController::checkConnexion');
 $routes->get('/accueil', 'Pages::Accueil');
 $routes->get('/absences', 'Pages::Absences');
 $routes->get('/rattrapages', 'Pages::Rattrapages');
--- app/Entities/User.php
+++ app/Entities/User.php
@@ -6,5 +6,69 @@ use CodeIgniter\Entity\Entity;
 
 class User extends Entity
 {
-    // ...
+    protected $attributes = [
+        'id' => null,
+        'first_name' => null,
+        'last_name' => null,
+        'email' => null,
+        'password' => null,
+        'type' => null,
+        'created_at' => null,
+        'updated_at' => null,
+    ];
+
+    public function __construct(array $data = [])
+    {
+        parent::__construct();
+
+        if (!empty($data) && is_array($data)) {
+            $this->fill($data);
+        }
+    }
+
+    public function getFullName()
+    {
+        return $this->attributes['first_name'] . ' ' . $this->attributes['last_name'];
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    public function setFirstName($value)
+    {
+        $this->attributes['first_name'] = $value;
+        return $this;
+    }
+
+    public function setLastName($value)
+    {
+        $this->attributes['last_name'] = $value;
+        return $this;
+    }
+
+    public function setEmail($value)
+    {
+        $this->attributes['email'] = $value;
+        return $this;
+    }
+
+    public function setPassword($value)
+    {
+        $this->attributes['password'] = $value;
+        return $this;
+    }
+
+    public function setType($value)
+    {
+        $this->attributes['type'] = $value;
+        return $this;
+    }
+
 }
